69b1e6d484bc229e092a03285dcc1d4797eb908b
theherrdani <sipos.herr.dani@gmail.com>
2018-11-06 23:00:17 +0100

    webservice communication initialization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
+import com.loxon.javachallenge2017.client.ClientEndpoint;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.websocket.ClientEndpointConfig;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.WebSocketContainer;
+import javax.xml.bind.DatatypeConverter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("initial");
+    public static void main(String[] args) throws IOException, DeploymentException {
+        WebSocketContainer webSocket = ContainerProvider.getWebSocketContainer();
+        ClientEndpointConfig.Configurator configurator = new ClientEndpointConfig.Configurator() {
+            @Override
+            public void beforeRequest(Map<String, List<String>> headers) {
+                headers.put("Authorization", Arrays.asList("Basic " + DatatypeConverter.printBase64Binary("userid:thejavan-1".getBytes())));
+            }
+        };
+        ClientEndpointConfig config = ClientEndpointConfig.Builder.create().configurator(configurator).build();
+
+        webSocket.connectToServer(ClientEndpoint.class, config, URI.create("ws://javachallenge.loxon.hu:8080/JavaChallenge2017/websocket"));
+        System.in.read();
     }
 }
